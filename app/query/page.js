f0a0fb2f64ca1b287a207d6d54da618f6eb226e4
SohanKsheersagar <sohansksheersagar007@gmail.com>
2025-05-25 00:18:48 +0530

    feat: Added a link back to Registration form from query route

--- app/query/page.js
+++ app/query/page.js
@@ -1,12 +1,15 @@
 'use client'
 import SQLQueryBox from '../../components/SQLQueryBox'
-
+import Link from 'next/link'
 export default function QueryPage() {
   return (
     <div className="page-center">
       <div>
         <h1>Run Raw SQL Query</h1>
         <SQLQueryBox />
+        <Link href="/">
+          <button style={{ marginTop: '1rem' }}> Go to Registration form </button>
+        </Link>
       </div>
     </div>
   )
