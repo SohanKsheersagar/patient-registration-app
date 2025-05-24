bd709f27acd87d54fd6753a9155a09c599b4e527
SohanKsheersagar <sohansksheersagar007@gmail.com>
2025-05-24 22:58:37 +0530

    feat: Intialized PGlite with indexedDB and verify setup

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect } from 'react'
+import { getDb } from '../lib/database'
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Patient Registration App</h1>
-    </div>
-  );
+  useEffect(() => {
+    getDb().then((db) => {
+      if (db) console.log('Pglite DB initialized')
+    })
+  }, [])
+
+  return <h1>Patient Registration App</h1>
 }
